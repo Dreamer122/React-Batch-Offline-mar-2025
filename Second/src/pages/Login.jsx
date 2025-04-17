095ce73feaf5ffd3e68fedb28c26966f2be52fe4
dreamer infotech <infotechdreamer@gmail.com>
2025-04-17 13:04:51 +0530

    react hook form (login page)

--- Second/src/pages/Login.jsx
+++ Second/src/pages/Login.jsx
@@ -1,7 +1,68 @@
 import React from 'react'
-
+import {useForm} from "react-hook-form"
+import { DevTool } from '@hookform/devtools';
 export const Login = () => {
+  const form=useForm()
+  // console.log(form)
+  const {register,control,handleSubmit,formState}=form
+  // const {name,ref,onchange,onblur}=register
+  const {errors}=formState;
+ const submitForm=(data)=>{
+  console.log(data)
+ }
+
   return (
-    <div>Login</div>
+    <div>
+      <h3>LOGIN FORM</h3>
+      <div className='login-form w-1/2 mx-auto my-5 border text-center'>
+      <form action="" onSubmit={handleSubmit(submitForm)}>
+        <div>
+          <label htmlFor="email">Email</label>
+          <input type="email"  id="email"
+          {...register("email",{
+            required:true,
+            pattern:{
+              value:"",
+              message:""
+            }
+          })}
+          className='border m-4'/>
+          {
+            errors.email? errors.email.message:""
+          }
+        </div>
+        <div>
+          <label htmlFor="password">password</label>
+          <input type="password" 
+          {...register("password",{
+            required:{
+              value:true,
+              message:"password is required"
+            },
+            minLength:{
+              value:5,
+              message:"min 5 char required"
+            }
+          })}
+          id="password" className='border m-4' />
+          <span>
+
+          {
+            errors.password?errors.password.message:""
+          }
+          </span>
+        </div>
+
+        <div>
+          <input type="submit" value="login" className='border-pink-700 border-2 rounded px-4 py-2 cursor-pointer' />
+        </div>
+
+
+      </form>
+      </div>
+     
+      <DevTool control={control} placement="top-right" />
+
+    </div>
   )
 }
